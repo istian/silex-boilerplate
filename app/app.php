7d67f64178c58f765ecee826f2697ca6bc783a9e
iamist <vicentechristian@gmail.com>
2015-05-11 13:37:49 +0800

    adding eloquent ORM support for database operation

--- app/app.php
+++ app/app.php
@@ -1,17 +1,4 @@
 <?php
 
 $app = require_once __DIR__ . '/bootstrap.php';
-
-// use Todo\Controllers\TodoController;
-
-/*$app['todo.controller'] = $app->share(function() use ($app) {
-  return new TodoController($app);
-});
-
-$app->get('/', 'todo.controller::index');*/
-
-
-// $app->get('/', 'Todo\\Controllers\\TodoController::index');
-// $app->get('/show/{name}', 'Todo\\Controllers\\TodoController::show');
-
 return $app;
--- app/src/Application/Config/services.php
+++ app/src/Application/Config/services.php
@@ -13,6 +13,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => $v
 // Url Generator
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
+// eloquent orm via capsule
+$app->register(new Ziadoz\Silex\Provider\CapsuleServiceProvider, [
+   'capsule.connection' => [
+       'driver' => 'mysql',
+       'host' => '127.0.0.1',
+       'database' => 'silexangular_development',
+       'username' => 'root',
+       'password' => getenv('IAMIST_DATABASE_PASSWORD'),
+       'charset' => 'utf8',
+       'collation' => 'utf8_unicode_ci',
+       'logging' => true
+   ]
+]);
+
 // Custom service example
 /*$app['example_service'] = function() {
     return new \Services\ExampleService();
